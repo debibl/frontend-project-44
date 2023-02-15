9bfede96c6fb14d9958fc9c31575c31a89065765
Vasilina <miryan.vasilina@gmail.com>
2023-02-15 18:19:13 +0300

    edit cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const welcomeMessage = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+};
+
+export default welcomeMessage;
