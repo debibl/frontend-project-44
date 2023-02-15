425b120d94bb12fe9b7c0ff338c4ddd033abb650
Vasilina <miryan.vasilina@gmail.com>
2023-02-16 02:40:28 +0300

    fix counter, parity check and typos

--- src/games-content/brain-even-logic.js
+++ src/games-content/brain-even-logic.js
@@ -4,14 +4,13 @@ const brainEven = () => {
   // welcome message
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hi, ${userName}!`);
 
   // task run
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   // answers counter
-  let i = 0;
-  while (i < 3) {
+  for (let i = 0; i < 3; i += 1) {
     const randomInt = Math.ceil(Math.random() * 100);
     console.log(`Question: ${randomInt}`);
     const userAnswer = readlineSync.question('Your answer: ');
@@ -22,13 +21,20 @@ const brainEven = () => {
         console.log('Correct!');
       } else {
         console.log(
-          `'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`,
+          `'${userAnswer}' is the wrong answer ;(. The correct answer was 'yes'.\nLet's try again, ${userName}!`,
+        );
+        return;
+      }
+    } else if (randomInt % 2 !== 0) {
+      if (userAnswer === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(
+          `'${userAnswer}' is the wrong answer ;(. The correct answer was 'no'.\nLet's try again, ${userName}!`,
         );
         return;
       }
     }
-
-    i += 1;
   }
 };
 
