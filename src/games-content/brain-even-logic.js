87709fce1a3d72bed3741b33fad816e2967f8abd
Vasilina <miryan.vasilina@gmail.com>
2023-02-16 03:02:29 +0300

    refactor code

--- src/games-content/brain-even-logic.js
+++ src/games-content/brain-even-logic.js
@@ -5,35 +5,26 @@ const brainEven = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hi, ${userName}!`);
-
-  // task run
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   // answers counter
-  for (let i = 0; i < 3; i += 1) {
+  let answersCount = 0;
+  while (answersCount < 3) {
     const randomInt = Math.ceil(Math.random() * 100);
     console.log(`Question: ${randomInt}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    // parity check
-    if (randomInt % 2 === 0) {
-      if (userAnswer === 'yes') {
-        console.log('Correct!');
-      } else {
-        console.log(
-          `'${userAnswer}' is the wrong answer ;(. The correct answer was 'yes'.\nLet's try again, ${userName}!`,
-        );
-        return;
-      }
-    } else if (randomInt % 2 !== 0) {
-      if (userAnswer === 'no') {
-        console.log('Correct!');
-      } else {
-        console.log(
-          `'${userAnswer}' is the wrong answer ;(. The correct answer was 'no'.\nLet's try again, ${userName}!`,
-        );
-        return;
-      }
+    // answer check
+    const isEven = (num) => num % 2 === 0;
+    const rightAnswer = isEven(randomInt) ? 'yes' : 'no';
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      answersCount += 1;
+    } else {
+      console.log(
+        `"${userAnswer}" is the wrong answer ;(. The correct answer was "${rightAnswer}".\nLet's try again, ${userName}!`,
+      );
+      return;
     }
   }
 };
