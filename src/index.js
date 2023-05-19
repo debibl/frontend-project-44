eba8a688760d748fb5fe4dee4f7bb2775f143504
Vasilina <miryan.vasilina@gmail.com>
2023-05-19 19:45:17 +0300

    fix brain-prime game AGAIN

--- src/index.js
+++ src/index.js
@@ -101,8 +101,8 @@ const prime = () => {
   const randomInt = randomIntGenerator(maxNumber);
   const task = `${randomInt}`;
   const primeNumbers = [
-    1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
-    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
   ];
   const isPrime = primeNumbers.includes(randomInt);
   const rightAnswer = isPrime ? 'yes' : 'no';
